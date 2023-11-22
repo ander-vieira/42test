2a95a58896289153ef473d0197318c8675dc92c8
andeviei <andeviei@student.42urduliz.com>
2023-11-22 20:38:39 +0100

    Upload to vogsphere

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,18 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 00:36:53 by andeviei          #+#    #+#             */
+/*   Created: 2023/09/13 10:08:19 by andeviei          #+#    #+#             */
 /*   Updated: 2023/09/18 17:46:55 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-int	ft_isalpha(int c)
-{
-	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
-}
+# include <unistd.h>
+# include <stdlib.h>
+
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
+
+# define FD_LIMIT 4096
+
+typedef struct s_buf {
+	char	b[BUFFER_SIZE];
+	ssize_t	l;
+}	t_buf;
+
+typedef struct s_str {
+	char	*d;
+	ssize_t	l;
+}	t_str;
+
+void	gnl_memcpy(char *dest, char *src, ssize_t size);
+ssize_t	find_newline(char *buffer, ssize_t chunk_len);
+
+char	*get_next_line(int fd);
+
+#endif
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,35 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 13:06:17 by andeviei          #+#    #+#             */
-/*   Updated: 2023/09/16 20:54:35 by andeviei         ###   ########.fr       */
+/*   Created: 2023/09/13 10:08:16 by andeviei          #+#    #+#             */
+/*   Updated: 2023/09/18 17:46:55 by andeviei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "get_next_line_bonus.h"
 
-char	*ft_strnstr(const char *h, const char *n, size_t len)
+void	gnl_memcpy(char *dest, char *src, ssize_t size)
 {
-	size_t	i;
-	size_t	n_len;
-	size_t	h_len;
+	ssize_t	i;
 
-	if (len == 0 && h == NULL)
-		return (NULL);
-	n_len = ft_strlen(n);
-	if (n_len == 0)
-		return ((char *)h);
-	h_len = ft_strlen(h);
 	i = 0;
-	while (i + n_len <= len && i + n_len <= h_len)
+	while (i < size)
 	{
-		if (!ft_strncmp(h + i, n, n_len))
-			return ((char *)h + i);
+		dest[i] = src[i];
 		i++;
 	}
-	return (NULL);
+}
+
+ssize_t	find_newline(char *buffer, ssize_t chunk_len)
+{
+	ssize_t	i;
+
+	i = 0;
+	while (i < chunk_len)
+	{
+		if (buffer[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (-1);
 }
