07c462ef9189876474a31748d5bf7eeac0580e39
andeviei <andeviei@student.42urduliz.com>
2023-11-30 12:34:25 +0100

    Upload to vogsphere

---
+++ pipex_bonus.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 17:01:46 by andeviei          #+#    #+#             */
+/*   Updated: 2023/11/25 00:19:09 by andeviei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+
+# include "libft/libft.h"
+
+# define PATH_PREF	"PATH="
+# define PATH_DELIM	'/'
+# define ENV_DELIM	':'
+
+typedef struct s_cmd {
+	char	*cmd;
+	int		fd_in;
+	int		fd_out;
+}	t_cmd;
+
+typedef struct s_pipex {
+	t_bool	here;
+	char	*pname;
+	char	*infile;
+	size_t	cmd_num;
+	t_cmd	*cmds;
+	char	*outfile;
+	char	**env;
+}	t_pipex;
+
+void	av_printusage(char *pname);
+void	av_printerror(char *pname, char *func, char *msg);
+
+char	**av_getargv(t_pipex *px, size_t i);
+void	av_freeargv(char **argv);
+
+t_fd	av_heredoc(t_pipex *px);
+
+t_bool	av_runcmd(t_pipex *px, size_t i);
+
+t_bool	av_initpipex(t_pipex *px, int argc, char **argv, char **envp);
+void	av_freepipex(t_pipex *px);
+
+char	*av_getpath(char *cmd, t_pipex *px);
+
+#endif
---
+++ runcmd.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   runcmd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 14:00:09 by andeviei          #+#    #+#             */
+/*   Updated: 2023/11/26 18:00:00 by andeviei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static t_bool	av_updatefdin(t_pipex *px, size_t i)
+{
+	if (px->cmds[i].fd_in == -1)
+		px->cmds[i].fd_in = open(px->infile, O_RDONLY);
+	if (px->cmds[i].fd_in == -1)
+		return (av_printerror(px->pname, px->infile, NULL), FALSE);
+	if (dup2(px->cmds[i].fd_in, STDIN_FILENO) == -1)
+		return (av_printerror(px->pname, "dup2", NULL),
+			close(px->cmds[i].fd_in), FALSE);
+	return (close(px->cmds[i].fd_in), TRUE);
+}
+
+static t_bool	av_updatefdout(t_pipex *px, size_t i)
+{
+	if (px->cmds[i].fd_out == -1)
+		px->cmds[i].fd_out = open(px->outfile,
+				O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (px->cmds[i].fd_out == -1)
+		return (av_printerror(px->pname, px->outfile, NULL), FALSE);
+	if (dup2(px->cmds[i].fd_out, STDOUT_FILENO) == -1)
+		return (av_printerror(px->pname, "dup2", NULL),
+			close(px->cmds[i].fd_out), FALSE);
+	return (close(px->cmds[i].fd_out), TRUE);
+}
+
+static int	av_runcmd_child(t_pipex *px, size_t i)
+{
+	char	**argv;
+	char	*pname;
+
+	if (!av_updatefdin(px, i))
+		return (av_freepipex(px), EXIT_FAILURE);
+	if (!av_updatefdout(px, i))
+		return (av_freepipex(px), EXIT_FAILURE);
+	argv = av_getargv(px, i);
+	if (argv == NULL)
+		return (av_freepipex(px), EXIT_FAILURE);
+	pname = av_getpath(argv[0], px);
+	if (pname == NULL)
+		return (av_freeargv(argv), av_freepipex(px), EXIT_FAILURE);
+	execve(pname, argv, px->env);
+	av_printerror(px->pname, "execve", NULL);
+	return (free(pname), av_freeargv(argv), av_freepipex(px), EXIT_FAILURE);
+}
+
+t_bool	av_runcmd(t_pipex *px, size_t i)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == -1)
+		return (av_printerror(px->pname, "fork", NULL), FALSE);
+	else if (pid == 0)
+		exit(av_runcmd_child(px, i));
+	else
+	{
+		if (px->cmds[i].fd_in != -1)
+			close(px->cmds[i].fd_in);
+		if (px->cmds[i].fd_out != -1)
+			close(px->cmds[i].fd_out);
+		waitpid(pid, &status, 0);
+		if (status != 0)
+			return (FALSE);
+		return (TRUE);
+	}
+}
