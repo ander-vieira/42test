2afda93950a9dc5c16180a91fdd15071115b2ce3
andeviei <andeviei@student.42urduliz.com>
2024-03-06 16:16:27 +0100

    Upload to vogsphere

---
+++ libft/misc/ft_parseint.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parseint.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andeviei <andeviei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 14:44:37 by andeviei          #+#    #+#             */
+/*   Updated: 2024/03/01 01:30:00 by andeviei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include "../libft_int.h"
+
+static size_t	ft_parseint_validbase(char *base)
+{
+	size_t	base_len;
+	size_t	i;
+
+	base_len = ft_strlen(base);
+	i = 0;
+	while (i < base_len)
+	{
+		if (ft_strchr(base + i + 1, base[i]) != -1)
+			return (0);
+		i++;
+	}
+	return (base_len);
+}
+
+static t_bool	ft_parseint_getsign(char *str, size_t *i)
+{
+	*i = 0;
+	while (ft_isspace(str[*i]))
+		*i += 1;
+	if (str[*i] == '-')
+		return (*i += 1, TRUE);
+	else if (str[*i] == '+')
+		return (*i += 1, FALSE);
+	else
+		return (FALSE);
+}
+
+static t_bool	ft_parseint_overflow(long result,
+	long pos, long base_len, t_bool sign)
+{
+	if (sign)
+		return (result > ((long)FT_INT_INTMAX + 1 - pos) / base_len);
+	else
+		return (result > ((long)FT_INT_INTMAX - pos) / base_len);
+}
+
+static int	ft_parseint_getnum(char *str, char *base, size_t i, t_bool sign)
+{
+	int		result;
+	int		base_len;
+	int		pos;
+	t_bool	empty;
+
+	base_len = (int)ft_parseint_validbase(base);
+	if (base_len < 2)
+		return (ft_seterror(FTERR_PARSENUM_BADBASE), 0);
+	empty = TRUE;
+	result = 0;
+	while (str[i] != '\0')
+	{
+		pos = ft_strchr(base, str[i]);
+		if (pos == -1)
+			return (ft_seterror(FTERR_PARSENUM_BADCHAR), result);
+		if (ft_parseint_overflow(result, pos, base_len, sign))
+			return (ft_seterror(FTERR_PARSENUM_OVERFLOW), 0);
+		result = result * base_len + pos;
+		empty = FALSE;
+		i++;
+	}
+	if (empty)
+		return (ft_seterror(FTERR_PARSENUM_EMPTY), 0);
+	return (ft_seterror(FTERR_OK), result);
+}
+
+int	ft_parseint(char *str, char *base)
+{
+	int		result;
+	t_bool	sign;
+	size_t	i;
+
+	sign = ft_parseint_getsign(str, &i);
+	result = ft_parseint_getnum(str, base, i, sign);
+	if (sign)
+		return (-result);
+	return (result);
+}
